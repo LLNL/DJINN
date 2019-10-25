7ac70a7c74874d1e64a322bb2244db3347c5d236
Kelli Humbird <humbird1@llnl.gov>
2019-10-25 10:52:00 -0700

    py3+tf2 compatible

--- tests/djinn_classification_example.py
+++ tests/djinn_classification_example.py
@@ -27,7 +27,6 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
-import cPickle
 try: from sklearn.model_selection import train_test_split
 except: from sklearn.cross_validation import train_test_split
 
